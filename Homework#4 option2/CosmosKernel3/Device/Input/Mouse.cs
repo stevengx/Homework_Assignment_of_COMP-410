9686d195c3cf2e52bb12ecb6c3df7ffbcc465f4f
stevengx <stevengx@163.com>
2015-11-23 19:10:52 -0600

    homework#4

---
+++ Homework#4 option2/CosmosKernel3/Device/Input/Mouse.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.Core;
+using Cosmos.Hardware.Drivers.PCI.Video;
+using Cosmos.Hardware;
+
+namespace CosmosKernel1.Devices.Input
+{
+    public class Mouse
+    {
+        //Underlying mouse driver instance - PS2 mice only but VMWare simulates PS2 for us.
+        private Cosmos.Hardware.Mouse mouse;
+
+        //Mouse X coord.
+        public ushort X
+        {
+            get
+            {
+                return (ushort)mouse.X;
+            }
+        }
+        //Mouse Y coord.
+        public ushort Y
+        {
+            get
+            {
+                return (ushort)mouse.Y;
+            }
+        }
+
+        //Screen width - sets max X coord.
+        public uint ScreenWidth
+        {
+            get
+            {
+                return mouse.ScreenWidth;
+            }
+            set
+            {
+                mouse.ScreenWidth = value;
+            }
+        }
+        //Screen height - sets max Y coord.
+        public uint ScreenHeight
+        {
+            get
+            {
+                return mouse.ScreenHeight;
+            }
+            set
+            {
+                mouse.ScreenHeight = value;
+            }
+        }
+
+        public Mouse(uint screenWidth, uint screenHeight)
+        {
+            //Create and initialise the underlying driver
+            mouse = new Cosmos.Hardware.Mouse();
+            mouse.Initialize(screenWidth, screenHeight);
+        }
+
+        //Store the last drwan position and pixel colours of the mouse so they can be reset when the mouse moves (causes max 1 frame lag in colours)
+        private int lastDrawX = 0;
+        private int lastDrawY = 0;
+        public int LastDrawX
+        {
+            get
+            {
+                return lastDrawX;
+            }
+        }
+        public int LastDrawY
+        {
+            get
+            {
+                return lastDrawY;
+            }
+        }
+        private uint[][] lastDrawPosCols = new uint[20][]
+            {
+                new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],
+                new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],new uint[20],
+            };
+        //Sets the colours for the mouse - 0 is black and can be used as transparent in draw method.
+        //20 x 20 pixels allowed for mouse design.
+        private uint[][] mouseCols = new uint[20][]
+        {
+            new uint[20] { 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x00FFFFFFFF, 0x00FFFFFFFF, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 },
+            new uint[20] { 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000011, 0x0000000011, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000, 0x0000000000 }
+        };
+        //Draws the mouse
+        public void Draw(VMWareSVGAII vgaDriver)
+        {
+            //If the mouse hasn't moved, don't reset.
+            if (lastDrawX != X || lastDrawY != Y)
+            {
+                //Clear the old mouse draw location.
+                DoDraw(lastDrawX, lastDrawY, vgaDriver, lastDrawPosCols, true);
+
+                //Store the existing colours where the mouse will now be.
+                for (ushort x = 0; x < 20; x++)
+                {
+                    for (ushort y = 0; y < 20; y++)
+                    {
+                        lastDrawPosCols[y][x] = vgaDriver.GetPixel((ushort)(X + x), (ushort)(Y + y));
+                    }
+                }
+                
+                //Updtae the last draw mouse position to the new position
+                lastDrawX = X;
+                lastDrawY = Y;
+            }
+            
+            //Draw the mouse in it's current position - ensure it's always on top.
+            DoDraw(X, Y, vgaDriver, mouseCols, false);
+        }
+        //Actually does the draw.
+        private void DoDraw(int X, int Y, VMWareSVGAII vgaDriver, uint[][] mouseCol, bool allowBlack)
+        {
+            for (ushort x = 0; x < 20; x++)
+            {
+                for (ushort y = 0; y < 20; y++)
+                {
+                    if (allowBlack || mouseCol[y][x] != 0)
+                    {
+                        vgaDriver.SetPixel((ushort)(X + x), (ushort)(Y + y), mouseCol[y][x]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Homework#4 option2/CosmosKernel3/Kernel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Cosmos.Hardware;
+
+namespace CosmosKernel3
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static uint old_x = 0;
+        public static uint old_y = 0;
+        public static VGAScreen VScreen = new VGAScreen();
+        public static Mouse m = new Cosmos.Hardware.Mouse();
+        protected override void BeforeRun()
+        {
+
+
+
+            Console.WriteLine("Vga Driver Booting");
+            VScreen.SetGraphicsMode(VGAScreen.ScreenSize.Size320x200, VGAScreen.ColorDepth.BitDepth8);
+            VScreen.Clear(0);
+            Console.WriteLine("Vga Driver Booted");
+
+            m.Initialize(320, 200);
+        }
+
+        public static void DrawMouse()
+        {
+
+            if (old_x == 0 && old_y == 0)
+            {
+                Do_DrawMouse();
+            }
+
+            if (old_x != m.X || old_y != m.Y)
+            {
+                //Clear the old mouse draw location.
+                VScreen.Clear(0);
+                //Draw a new one
+                Do_DrawMouse();
+                //Updtae the last draw mouse position to the new position
+                old_x = (uint)m.X;
+                old_y = (uint)m.Y;
+            }
+
+        }
+
+        public static void Do_DrawMouse()
+        {
+            VScreen.SetPixel320x200x8((uint)m.X, (uint)m.Y, 63);
+            VScreen.SetPixel320x200x8((uint)m.X + 1, (uint)m.Y, 63);
+            VScreen.SetPixel320x200x8((uint)m.X + 2, (uint)m.Y, 63);
+            VScreen.SetPixel320x200x8((uint)m.X, (uint)m.Y + 1, 63);
+            VScreen.SetPixel320x200x8((uint)m.X, (uint)m.Y + 2, 63);
+            VScreen.SetPixel320x200x8((uint)m.X + 1, (uint)m.Y + 1, 63);
+            VScreen.SetPixel320x200x8((uint)m.X + 2, (uint)m.Y + 2, 63);
+            VScreen.SetPixel320x200x8((uint)m.X + 3, (uint)m.Y + 3, 63);
+        }
+
+
+        protected override void Run()
+        {
+            DrawMouse();
+        }
+    }
+}
